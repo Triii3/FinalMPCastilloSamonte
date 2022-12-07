f9182e1b455e72593efd15c09684a5c116d64e1f
Triii3 <castilloreynaldo3@gmail.com>
2022-12-07 20:41:36 +0800

    added jackenpoy layout that will open when img is pressed. a home icon in the layout to go back to home. code for jackenpoy in java class. popoutlayout when user wins/lose

--- app/src/main/java/com/example/finalmpcastillosamonte/MainActivity.java
+++ app/src/main/java/com/example/finalmpcastillosamonte/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.finalmpcastillosamonte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgJackEnPoy  =(ImageView)findViewById(R.id.imgJackEnPoy);
+
+        imgJackEnPoy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent jepAct = new Intent(getApplicationContext(), JackEnPoy.class);
+                startActivity(jepAct);
+            }
+        });
 
 /*
         Happy Birthday! Goodmorning!
